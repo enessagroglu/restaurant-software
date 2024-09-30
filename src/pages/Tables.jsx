5b144c01edb9ea9413a88bd47a19e3c97393332c
Enes Sağıroğlu <enes-2001sagroglu@hotmail.com>
2024-09-30 15:46:28 +0300

    masalar modal yapısı kuruldu

--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -1,6 +1,7 @@
-import { Card } from "antd";
+import { Card, Modal, Button } from "antd";
 import { useSelector, useDispatch } from 'react-redux';
 import { setTableOccupied, setTableEmpty, setTableClosed, setTableReopened } from '../store/Tables';
+import { useState } from 'react';
 
 import masa1 from "../assets/numbers/1.png";
 import masa2 from "../assets/numbers/2.png";
@@ -14,116 +15,72 @@ import masa9 from "../assets/numbers/9.png";
 import masa10 from "../assets/numbers/10.png";
 
 export default function Tables() {
-
   const { Meta } = Card;
-
   const dispatch = useDispatch();
-
   const tables = useSelector((state) => state.tables);
 
-  const handleSetOccupied = (tableNumber) => {
-    dispatch(setTableOccupied({ tableNumber }));
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTable, setSelectedTable] = useState(null);
+
+  const handleOpenModal = (tableNumber) => {
+    setSelectedTable(tableNumber);
+    setIsModalOpen(true);
   };
 
-  const handleSetEmpty = (tableNumber) => {
-    dispatch(setTableEmpty({ tableNumber }));
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedTable(null);
   };
 
-  const handleSetClosed = (tableNumber) => {
-    dispatch(setTableClosed({ tableNumber }));
-  };
-
-  const handleReopenTable = (tableNumber) => {
-    dispatch(setTableReopened({ tableNumber }));
-  };
-
+  const tableData = [
+    { img: masa1, title: "Masa 1", status: tables.status1 },
+    { img: masa2, title: "Masa 2", status: tables.status2 },
+    { img: masa3, title: "Masa 3", status: tables.status3 },
+    { img: masa4, title: "Masa 4", status: tables.status4 },
+    { img: masa5, title: "Masa 5", status: tables.status5 },
+    { img: masa6, title: "Masa 6", status: tables.status6 },
+    { img: masa7, title: "Masa 7", status: tables.status7 },
+    { img: masa8, title: "Masa 8", status: tables.status8 },
+    { img: masa9, title: "Masa 9", status: tables.status9 },
+    { img: masa10, title: "Masa 10", status: tables.status10 },
+  ];
 
   return (
     <div>
       <h1>Masalar</h1>
-
       <div className="table-layout">
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa1" src={masa1} />}
-        >
-          <Meta title="Masa 1" description={`status: ${tables.status1}`} />
-          
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa2" src={masa2} />}
-        >
-          <Meta title="Masa 2" description={`status: ${tables.status2}`} />
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa3" src={masa3} />}
-        >
-          <Meta title="Masa 3" description={`status: ${tables.status3}`} />
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa4" src={masa4} />}
-        >
-          <Meta title="Masa 4" description={`status: ${tables.status4}`} />
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa5" src={masa5} />}
-        >
-          <Meta title="Masa 5" description={`status: ${tables.status5}`} />
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa6" src={masa6} />}
-        >
-          <Meta title="Masa 6" description={`status: ${tables.status6}`} />
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa7" src={masa7} />}
-        >
-          <Meta title="Masa 7" description={`status: ${tables.status7}`} />
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa8" src={masa8} />}
-        >
-          <Meta title="Masa 8" description={`status: ${tables.status8}`} />
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa9" src={masa9} />}
-        >
-          <Meta title="Masa 9" description={`status: ${tables.status9}`} />
-        </Card>
-
-        <Card
-          hoverable
-          style={{ width: 200 }}
-          cover={<img alt="masa10" src={masa10} />}
-        >
-          <Meta title="Masa 10" description={`status: ${tables.status10}`} />
-        </Card>
+        {tableData.map((table, index) => (
+          <Card
+            key={index}
+            hoverable
+            style={{ width: 200 }}
+            cover={<img alt={table.title} src={table.img} />}
+            onClick={() => handleOpenModal(index + 1)}
+          >
+            <Meta title={table.title} description={`status: ${table.status}`} />
+          </Card>
+        ))}
       </div>
+
+      <Modal
+        title={`Masa ${selectedTable}`}
+        open={isModalOpen}
+        onOk={handleCloseModal}
+        onCancel={handleCloseModal}
+        footer={[
+          <Button key="back" onClick={handleCloseModal}>
+            Kapat
+          </Button>,
+        ]}
+      >
+        {selectedTable && (
+          <div>
+            <p>Masa {selectedTable} bilgileri burada görüntülenebilir.</p>
+            <p>Durum: {tables[`status${selectedTable}`]}</p>
+            
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
