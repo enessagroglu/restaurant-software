56926c5be91a9f95d6dfb4645e3e7578a7b6c1b0
Enes Sağıroğlu <enes-2001sagroglu@hotmail.com>
2024-10-18 20:04:14 +0300

    menu iskeleti hazır, slata ve içecekler kaldı sadece

--- src/pages/MenuContent/RestaurantMenu.jsx
+++ src/pages/MenuContent/RestaurantMenu.jsx
@@ -1,12 +1,50 @@
-
-
+import menuItems from "./menuItems";
+import { Divider } from 'antd';
 
 export default function RestaurantMenu() {
 
+  const groupTitles = {
+    "kapali-pide": "Kapalı Pideler",
+    "acik-pide": "Açık Pideler",
+    "yuvarlak-pide": "Yuvarlak Pideler",
+    salata: "Salatalar",
+    icecek: "İçecekler",
+  };
 
-    return(
-        <>
-        sex
-        </>
-    )
+  const groupBy = (items, key) => {
+    return items.reduce((result, item) => {
+      (result[item[key]] = result[item[key]] || []).push(item);
+      return result;
+    }, {});
+  };
+
+  const groupedMenuItems = groupBy(menuItems, "group");
+
+  return (
+    <>
+      <div>
+        <h1 style={{display:"flex", justifyContent:"center"}}>Menü</h1>
+        {Object.keys(groupedMenuItems).map((group, index) => (
+          <div key={index} className="menu-group">
+            {/* Eğer başlık eşlemesinde grup varsa onu kullan, yoksa grup adını göster */}
+            <h2>{groupTitles[group] || group.replace("-", " ").toUpperCase()}</h2> 
+            <Divider />
+            <div className="menu-container">
+              {groupedMenuItems[group].map((item, index) => (
+                <div key={index} className="menu-item">
+                  <h3 style={{color:"#15499c"}}>{item.name}</h3>
+                  <img className="menu-img" src={item.image} alt={item.name} />
+                  <p>{item.description}</p>
+                  <p>Fiyat: {item.price} TL</p>
+                  
+                  {/* Eğer group "icecek" değilse, içindekiler kısmını göster */}
+                  {item.group !== "icecek" && <p>İçindekiler: {item.ingredients}</p>}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
---
+++ src/pages/MenuContent/menuItems.jsx
@@ -0,0 +1,115 @@
+import kiymaliKapali from "../../assets/menuItems/kiymali-kapali.webp";
+import kavurmaliKapali from "../../assets/menuItems/kavurmali-kapali.webp";
+import pastirmaliKapali from "../../assets/menuItems/pastirmali-kapali.webp";
+import pastirmaliKasarliKapali from "../../assets/menuItems/pastirmali-kasarli-kapali.webp";
+
+import kavurmaliYuvarlak from "../../assets/menuItems/kavurmali-yuvarlak.webp";
+import kiymaliYuvarlak from "../../assets/menuItems/kiymali-yuvarlak.webp";
+import peynirliYuvarlak from "../../assets/menuItems/peynirli-yuvarlak.webp";
+import karisikYuvarlak from "../../assets/menuItems/karisik-yuvarlak.webp";
+
+import kavurmaliAcik from "../../assets/menuItems/kavurmali-acik.webp";
+import kiymaliAcik from "../../assets/menuItems/kiymali-acik.webp";
+import peynirliAcik from "../../assets/menuItems/peynirli-acik.webp";
+import karisikAcik from "../../assets/menuItems/karisik-acik.webp";
+
+const menuItems = [
+  {
+    name: "Kıymalı Kapalı Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Kıyma / Trabzon Tereyağı / Çıtır Hamur ",
+    group: "kapali-pide",
+    image: kiymaliKapali,
+    price: 200,
+  },
+  {
+    name: "Kavurmalı Kapalı Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Kavurma / Trabzon Tereyağı / Çıtır Hamur",
+    group: "kapali-pide",
+    image: kavurmaliKapali,
+    price: 200,
+  },
+  {
+    name: "Pastırmalı Kapalı Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Pastırma / Trabzon Tereyağı / Çıtır Hamur",
+    group: "kapali-pide",
+    image: pastirmaliKapali,
+    price: 220,
+  },
+  {
+    name: "Pastırmalı Kaşarlı Kapalı Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Pastırma / Kaşar / Trabzon Tereyağı / Çıtır Hamur",
+    group: "kapali-pide",
+    image: pastirmaliKasarliKapali,
+    price: 200,
+  },
+  {
+    name: "Kavurmalı Yuvarlak Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Kavurma / Trabzon Tereyağı / Çıtır Hamur ",
+    group: "yuvarlak-pide",
+    image: kavurmaliYuvarlak,
+    price: 190,
+  },
+  {
+    name: "Kıymalı Yuvarlak Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Kıyma / Trabzon Tereyağı / Çıtır Hamur",
+    group: "yuvarlak-pide",
+    image: kiymaliYuvarlak,
+    price: 190,
+  },
+  {
+    name: "Peynirli Yuvarlak Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Peynir / Trabzon Tereyağı / Çıtır Hamur",
+    group: "yuvarlak-pide",
+    image: peynirliYuvarlak,
+    price: 190,
+  },
+  {
+    name: "Karışık Yuvarlak Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Kavurma /Peynir / Trabzon Tereyağı / Çıtır Hamur",
+    group: "yuvarlak-pide",
+    image: karisikYuvarlak,
+    price: 190,
+  },
+  {
+    name: "Kavurmalı Açık Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Kavurma / Trabzon Tereyağı / Çıtır Hamur",
+    group: "acik-pide",
+    image: kavurmaliAcik,
+    price: 200,
+  },
+  {
+    name: "Kıymalı Açık Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Kıyma / Trabzon Tereyağı / Çıtır Hamur",
+    group: "acik-pide",
+    image: kiymaliAcik,
+    price: 200,
+  },
+  {
+    name: "Peynirli Açık Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Peynir / Trabzon Tereyağı / Çıtır Hamur",
+    group: "acik-pide",
+    image: peynirliAcik,
+    price: 200,
+  },
+  {
+    name: "Karışık Açık Pide",
+    description: "Parpali Klasikleri",
+    ingredients: "Kavurma / Peynir / Trabzon Tereyağı / Çıtır Hamur",
+    group: "acik-pide",
+    image: karisikAcik,
+    price: 200,
+  },
+];
+
+export default menuItems;
